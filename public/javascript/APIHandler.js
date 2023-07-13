7a963fe800fbc47be785419cc30f9300713b80bc
Matt Krebs <mattekrebs@gmail.com>
2023-07-13 21:34:39 +0200

    progress

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,55 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList(){
+    axios.get(`${this.BASE_URL}/characters`)
+    .then((all) => {
+      console.log('All Characters:', all.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then((character) => {
+      console.log('Character with id: ', character.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister(characterData){
+    axios.post(`${this.BASE_URL}/characters`, characterData)
+    .then((newChar) => {
+      console.log('Character created: ', newChar.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, characterData){
+    axios.put(`${this.BASE_URL}/characters/${id}`, characterData)
+    .then((updatedChar) => {
+      console.log('Character updated: ', updatedChar.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id){
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
+    .then(() => {
+      console.log('Deleted character: ', id);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,19 +1,48 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+const APIHandler = require('./APIHandler')
+
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    const characterId = document.getElementById('character-id').value
 
+    charactersAPI.getOneRegister(characterId)
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    const characterId = document.getElementById('character-id').value
 
+    charactersAPI.deleteOneRegister(characterId)
+      .then(() => {
+        document.getElementById('delete-one').style.backgroundColor = 'green'
+      })
+      .catch(() => {
+        document.getElementById('delete-one').style.backgroundColor = 'red'
   });
 
+  //need to talk to Ashref about the all of this..
+
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+ 
+  const id = document.getElementById('edit-id').value;
+  
+  const updatedCharacterData = {
+    name: document.getElementById('edit-name').value,
+    occupation: document.getElementById('edit-occupation').value,
+    weapon: document.getElementById('edit-weapon').value,
+  };
+
+  updateOneRegister(id, updatedCharacterData);
+
+  document.getElementById('edit-id').value = '';
+  document.getElementById('edit-name').value = '';
+  document.getElementById('edit-occupation').value = '';
+  document.getElementById('edit-weapon').value = '';
+});
 
   });
 
